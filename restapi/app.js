6198607f8f0d6ff6e169877eec7abbe49603df81
jbgraille@gmail.com <jbgraille@gmail.com>
2020-07-02 20:22:19 +0200

    posting data

--- restapi/app.js
+++ restapi/app.js
@@ -1,22 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 bookRouter.route('/books')
-  .get((req,res) => {
-    Book.find(
-      (err, books) => {
-        if (err){
-          return res.send(err);
-        } 
-        return res.json(books);
+  .post((req, res) => {
+    const book = new Book(req.body);
+    console.log(book);
+    book.save();
+    return res.status(201).json(book);
+  })
+  .get((req, res) => {
+    const { query } = req;
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
       }
+      return res.json(books);
+    }
     );
   });
+
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    }
+    );
+  });
+
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
--- restapi/booksJson.js
+++ restapi/booksJson.js
@@ -10,5 +10,17 @@ db.books.insert([
     isbn: "1935182722",
     pageCount: 592,
     categories: "Java"
+  },
+  {
+    title: "Android the best",
+    isbn: "1",
+    pageCount: 100,
+    categories: "Java"
+  },
+  {
+    title: "Android with a computer",
+    isbn: "2",
+    pageCount: 600,
+    categories: "Computer"
   }
 ])
