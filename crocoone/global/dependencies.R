e5a8d33f05966b3b112d129a322a58b25056d1e3
jbgraille <jbgraille@gmail.com>
2020-12-28 23:36:00 +0100

    here package

--- crocoone/global/dependencies.R
+++ crocoone/global/dependencies.R
@@ -1,5 +1,6 @@
 rm(list = ls(all.names = TRUE))
-dependencies <- c("rtweet","dplyr","ggplot2","tidytext", "echarts4r", "shiny")
+dependencies <- c("rtweet","dplyr","ggplot2",
+                  "tidytext", "echarts4r", "shiny","here")
 cranMirror <- "https://ftp.gwdg.de/pub/misc/cran/"
 new.packages <- dependencies[!(dependencies %in% installed.packages()[,"Package"])]
 if (length(new.packages)>0) install.packages(new.packages,repos = "https://ftp.fau.de/cran/")
