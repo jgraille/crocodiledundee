f974302bb9b740e4417ec10d84a7faa4d950c93f
jbgraille <jbgraille@jbgrailles-macbook-pro.home>
2020-12-28 23:18:09 +0100

    cranMirror url

--- crocoone/global/dependencies.R
+++ crocoone/global/dependencies.R
@@ -1,9 +1,7 @@
 rm(list = ls(all.names = TRUE))
 #gc()
-
 dependencies <- c("rtweet","dplyr","ggplot2","tidytext", "echarts4r", "shiny")
-
+cranMirror <- "https://ftp.gwdg.de/pub/misc/cran/"
 new.packages <- dependencies[!(dependencies %in% installed.packages()[,"Package"])]
-if (length(new.packages)>0) install.packages(new.packages)
-
-lapply(dependencies, library, character.only = TRUE)
+if (length(new.packages)>0) install.packages(new.packages,repos = "https://ftp.fau.de/cran/")
+lapply(dependencies, require, character.only = TRUE)
